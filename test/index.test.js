2cc3c113bba471067547337fa333490ce639a142
ameba23 <ameba23@systemli.org>
2020-01-14 22:36:22 +0100

    additional tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 const { describe } = require('tape-plus')
-const { packLabel, unpackLabel } = require('..')
+const { packLabel, unpackLabel, signingKeypair, signShard, openShard } = require('..')
 
 const secret = 'its nice to be important but its more important to be nice'
 const label = 'This is the password'
@@ -19,6 +19,30 @@ describe('pack and unpack', (context) => {
     assert.ok(Buffer.isBuffer(packed), 'returns a buffer')
     const unpacked = unpackLabel(packed)
     assert.equal(unpacked.secret.toString(), secret, 'secret is correct')
+    next()
+  })
+})
+
+describe('sign and verify', (context) => {
+  context('basic', (assert, next) => {
+    const shard = Buffer.from(secret) // TODO: real shard
+    const keypair = signingKeypair()
+    const signedShard = signShard(shard, keypair)
+    assert.ok(Buffer.isBuffer(signedShard), 'signed shard is a buffer')
+    const verifiedShard = openShard(signedShard, keypair.publicKey)
+    assert.true(verifiedShard, 'Shard was verified')
+    assert.equal(verifiedShard.toString('hex'), shard.toString('hex'), 'VerifiedShard is correct')
+    next()
+  })
+
+  context('bad public key', (assert, next) => {
+    const shard = Buffer.from(secret) // TODO: real shard
+    const keypair = signingKeypair()
+    const signedShard = signShard(shard, keypair)
+    assert.ok(Buffer.isBuffer(signedShard), 'signed shard is a buffer')
+    keypair.publicKey[0] = keypair.publicKey[0] === 1 ? 2 : 1
+    const verifiedShard = openShard(signedShard, keypair.publicKey)
+    assert.false(verifiedShard, 'Shard could not be verified')
     next()
   })
 })
